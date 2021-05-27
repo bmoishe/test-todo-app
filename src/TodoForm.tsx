30c9ec00ab8dfbdf1dc4edd7e4a4ea91002b5384
Moishe Barnes <moishebarnes@gmail.com>
2021-05-27 14:20:45 +0100

    Initialize project using Create React App

---
+++ src/TodoForm.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function TodoForm() {
+  return (
+    <form className="TodoForm">
+      <input type="text" placeholder='Add todo' />
+    </form>
+  );
+}
+
+export default TodoForm;
